9988a3dd38048c183d4239db791746dd027278e9
lautarograc <lautarograciani2106@gmail.com>
2022-05-14 20:40:36 -0300

    Revert "added users index for admin users"
    This reverts commit 394ecec58f07a50fcdf1e5d49e80195ea16c5da9.

--- app/admin/targets.rb
+++ app/admin/targets.rb
@@ -1,19 +1,19 @@
 ActiveAdmin.register Target do
-    menu :priority => 1, :label => "Targets"
-    permit_params :title, :radius, :latitude, :longitude, :topic_id, :user_id, :active
+  menu priority: 1, label: 'Targets'
+  permit_params :title, :radius, :latitude, :longitude, :topic_id, :user_id, :active
 
-    scope :all, :default => true
-    
-    form :html => { :multipart => true } do |f|
-        f.inputs "Target Details" do
-            f.input :title
-            f.input :radius
-            f.input :latitude
-            f.input :longitude
-            f.input :topic_id
-            f.input :user_id
-            f.input :active
-        end
-        f.actions
+  scope :all, default: true
+
+  form html: { multipart: true } do |f|
+    f.inputs 'Target Details' do
+      f.input :title
+      f.input :radius
+      f.input :latitude
+      f.input :longitude
+      f.input :topic_id
+      f.input :user_id
+      f.input :active
     end
+    f.actions
+  end
 end
